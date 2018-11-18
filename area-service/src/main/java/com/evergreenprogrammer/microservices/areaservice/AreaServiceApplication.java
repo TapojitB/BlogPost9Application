f0cd53ce5f590325484fa3ba6274c79015c2fc0a
Tapojit Bhattacharya <tapojit.bhattacharya@gmail.com>
2018-11-18 18:21:29 +0530

    added zipkin

--- area-service/src/main/java/com/evergreenprogrammer/microservices/areaservice/AreaServiceApplication.java
+++ area-service/src/main/java/com/evergreenprogrammer/microservices/areaservice/AreaServiceApplication.java
@@ -3,6 +3,9 @@ package com.evergreenprogrammer.microservices.areaservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @EnableDiscoveryClient(autoRegister = true)
 @SpringBootApplication
@@ -10,6 +13,12 @@ public class AreaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AreaServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+
 	}
 
 }
--- shape-service/src/main/java/com/evergreenprogrammer/microservices/shapeservice/ShapeServiceApplication.java
+++ shape-service/src/main/java/com/evergreenprogrammer/microservices/shapeservice/ShapeServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @EnableDiscoveryClient(autoRegister = true)
 @EnableFeignClients(basePackages = "com.evergreenprogrammer.microservices.shapeservice.proxy")
@@ -12,6 +15,12 @@ public class ShapeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShapeServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+
 	}
 
 }
